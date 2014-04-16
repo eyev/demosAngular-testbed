2ee601ea55b509bc2234aa59b63dbcc13ac39592
Justin Conklin <justin@eyev.org>
2014-04-16 00:15:47 -0400

    Search Bar
    Demo with a search bar that appears after a set amount of todo items
    have been submitted.  This example produces a bug.

---
+++ app/app.js
@@ -0,0 +1,27 @@
+﻿/*
+    Main app module
+*/
+
+var demoAngular = angular.module('demoAngular', [
+    'ngRoute',
+    'app.directives.itemDetail',
+    'app.services.itemService'
+]);
+
+demoAngular.config(function ($routeProvider) {
+    $routeProvider.when('/item',
+        {
+            templateUrl: 'app/controllers/templates/itemList.html',
+            controller: 'ItemListCtrl'
+        })
+    $routeProvider.when('/items',
+        {
+            templateUrl: 'app/controllers/templates/itemList.html',
+            controller: 'ItemListCtrl'
+        })
+    $routeProvider.when('/item/:itemId',
+        {
+            templateUrl: 'app/controllers/templates/itemDetail.html',
+            controller: 'ItemDetailCtrl'
+        })
+});
---
+++ app/directives/itemDetailDir.js
@@ -0,0 +1,23 @@
+﻿/*
+
+    Item Detail directive
+
+*/
+
+
+angular.module('app.directives.itemDetail', [])
+    .directive('itemDetail', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                item: '=',
+                removeItem: '&',
+                index: '='
+            },
+            replace: true,
+            transclude: true,
+            templateUrl: "app/directives/templates/itemDetailDir.html",
+            link: function (scope, element, attrs) {},
+            controller: function ($scope) {}
+        };
+    });
